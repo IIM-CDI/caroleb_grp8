81348b3c9fe974934f62e3d8db9d7fd437125409
Leogeox <leovginx@gmail.com>
2026-01-06 15:46:31 +0100

    style(Leo): style homepage

--- caroleb-theme/front-page.php
+++ caroleb-theme/front-page.php
@@ -1,60 +1,54 @@
 <?php get_header(); ?>
 
 <main>
-    <section class="post1">
+    <section class="accueil-post1">
         <?php
         $query = new WP_Query(array(
             'post_type' => 'post-acceuil',
             'p' => 227
         ));
-
         if ($query->have_posts()):
             while ($query->have_posts()):
                 $query->the_post();
-                the_title('<h2>', '</h2>');
+                the_title('<h1 class="accueil-title">', '</h1>');
                 the_content();
             endwhile;
         endif;
-
         wp_reset_postdata();
         ?>
     </section>
 
-    <section class="post2">
+    <section class="accueil-post2">
         <?php
         $query = new WP_Query(array(
             'post_type' => 'post-acceuil',
             'p' => 389
         ));
-
         if ($query->have_posts()):
             while ($query->have_posts()):
                 $query->the_post();
+                the_title('<h2 class="accueil-subtitle1">', '</h2>');
 
                 the_content();
             endwhile;
         endif;
-
         wp_reset_postdata();
         ?>
     </section>
 
-    <section class="post3">
-
+    <section class="accueil-post3">
         <?php
         $query = new WP_Query(array(
             'post_type' => 'post-acceuil',
             'p' => 391
         ));
-
         if ($query->have_posts()):
             while ($query->have_posts()):
                 $query->the_post();
-                the_title('<h2>', '</h2>');
+                the_title('<h2 class="accueil-subtitle2">', '</h2>');
 
             endwhile;
         endif;
-
         wp_reset_postdata();
         ?>
         <?php
@@ -67,7 +61,7 @@
         $img5 = get_field('image_5', $post_id);
         $img6 = get_field('image_6', $post_id);
         ?>
-        <div class="mosaic  oeuvre">
+        <div class="mosaic oeuvre">
             <div class="col left col2">
                 <?php if ($img1)
                     echo wp_get_attachment_image($img1, 'large'); ?>
@@ -106,17 +100,16 @@
 
             endwhile;
         endif;
-
         wp_reset_postdata();
         ?>
     </section>
-    <section class="post4">
+
+    <section class="accueil-post4">
         <?php
         $query = new WP_Query(array(
             'post_type' => 'post-acceuil',
             'p' => 394
         ));
-
         if ($query->have_posts()):
             while ($query->have_posts()):
                 $query->the_post();
@@ -124,7 +117,6 @@
 
             endwhile;
         endif;
-
         wp_reset_postdata();
         ?>
         <?php echo do_shortcode('[metaslider id="425"]'); ?>
@@ -133,7 +125,6 @@
             'post_type' => 'post-acceuil',
             'p' => 394
         ));
-
         if ($query->have_posts()):
             while ($query->have_posts()):
                 $query->the_post();
@@ -141,11 +132,9 @@
 
             endwhile;
         endif;
-
         wp_reset_postdata();
         ?>
     </section>
 
 </main>
-
 <?php get_footer(); ?>
