7f7cbddb1437e7b1ed9abd4088359e91e931a989
Leogeox <leovginx@gmail.com>
2026-01-06 16:13:43 +0100

    style(Leo): stylisation presse

--- caroleb-theme/page-presse.php
+++ caroleb-theme/page-presse.php
@@ -4,7 +4,7 @@ get_header();
 
 <main id="main-content" class="site-main">
     <section class="media-intro">
-        <h1> <?php the_title(); ?></h1>
+        <h1 class="presse-title"> <?php the_title(); ?></h1>
     </section>
     <?php the_content(); ?>
 </main>
